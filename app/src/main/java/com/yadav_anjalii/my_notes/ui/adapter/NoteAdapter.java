e6a561d0d0093448f3e95ebd6ddc009e9c5069d5
AnjaliiYadav <48989136+AnjaliiYadav@users.noreply.github.com>
2021-09-16 23:14:34 +0400

    Update NoteAdapter.java

--- app/src/main/java/com/yadav_anjalii/my_notes/ui/adapter/NoteAdapter.java
+++ app/src/main/java/com/yadav_anjalii/my_notes/ui/adapter/NoteAdapter.java
@@ -1,11 +1,14 @@
 package com.yadav_anjalii.my_notes.ui.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.yadav_anjalii.my_notes.databinding.NoteItemBinding;
 import com.yadav_anjalii.my_notes.model.Note;
 
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.List;
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
 
     List<Note> notes;
+    private Context context;
+    private ItemClickListener itemClickListener;
 
     public NoteAdapter(List<Note> notes) {
         this.notes = notes;
@@ -21,22 +26,46 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        NoteItemBinding binding= NoteItemBinding.inflate(inflater);
+        return new NoteViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NoteAdapter.NoteViewHolder holder, int position) {
-
+        holder.binding.setNotes(notes.get(position));
+        holder.binding.executePendingBindings();
     }
 
     @Override
     public int getItemCount() {
+        if (notes != null)
+            return notes.size();
         return 0;
     }
 
-    public class NoteViewHolder extends RecyclerView.ViewHolder {
-        public NoteViewHolder(@NonNull View itemView) {
-            super(itemView);
+    public class NoteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        NoteItemBinding binding;
+        public NoteViewHolder(@NonNull NoteItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            binding.getRoot().setOnClickListener(this::onClick);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (itemClickListener != null)
+                itemClickListener.itemOnClick(view, notes.get(getAdapterPosition()));
         }
     }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener{
+        void itemOnClick(View view, Note note);
+    }
+
 }
