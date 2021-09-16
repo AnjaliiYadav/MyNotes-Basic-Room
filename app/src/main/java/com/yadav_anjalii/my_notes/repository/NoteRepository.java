4cc4c36da9bef7a3fe40c88c7d4520bf854d7067
AnjaliiYadav <48989136+AnjaliiYadav@users.noreply.github.com>
2021-09-16 07:50:24 +0400

    Update NoteRepository.java

--- app/src/main/java/com/yadav_anjalii/my_notes/repository/NoteRepository.java
+++ app/src/main/java/com/yadav_anjalii/my_notes/repository/NoteRepository.java
@@ -1,5 +1,34 @@
 package com.yadav_anjalii.my_notes.repository;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.yadav_anjalii.my_notes.db.NoteDatabase;
+import com.yadav_anjalii.my_notes.model.Note;
+
+import java.util.List;
+
 public class NoteRepository {
-private NoteRepository repository;
+
+    private NoteDatabase noteDatabase;
+    public NoteRepository(Context context) {
+        noteDatabase = NoteDatabase.getDatabase(context);
+    }
+
+    public void insertNote(Note note){
+        noteDatabase.noteDao().insertNote(note);
+    }
+
+    public void deleteNote(Note note){
+        noteDatabase.noteDao().deleteNote(note);
+    }
+
+    public void updateNote(Note note){
+        noteDatabase.noteDao().deleteNote(note);
+    }
+
+    public LiveData<List<Note>> getNotes(){
+       return noteDatabase.noteDao().getAllNotes();
+    }
 }
